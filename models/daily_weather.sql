016a396b8cf42b5f418d691911f5f1f7fcde9588
amitesh@botconsulting.io <amitesh@botconsulting.io>
2026-04-23 11:25:57 +0000

    agg_weather_commit

--- models/daily_weather.sql
+++ models/daily_weather.sql
@@ -11,7 +11,14 @@ with daily_weather as (
      from {{ source('demo', 'weather') }}
 ),
 daily_weather_agg as (
-    select daily_weather, weather, count(weather) from daily_weather group by daily_weather, weather
+    select daily_weather, weather,
+    round(avg(temp),2) as avg_temp,
+    round(avg(pressure),2) as avg_pressure,
+    round(avg(humidity),2) as avg_humidity,
+    round(avg(clouds),2) as avg_clouds
+    -- row_number() over(partition by daily_weather order by count(weather) desc) as row_number
+    from daily_weather group by daily_weather, weather
+    qualify row_number() over(partition by daily_weather order by count(weather) desc) =1
 )
 
 select * from daily_weather_agg
