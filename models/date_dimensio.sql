fa55697f2b2adc0b66e08172239ecac300a6fb30
amitesh@botconsulting.io <amitesh@botconsulting.io>
2026-04-23 08:59:40 +0000

    date_dimension_model

--- models/date_dimensio.sql
+++ models/date_dimensio.sql
@@ -2,6 +2,22 @@ with cte as (
     select 
     to_timestamp(started_at) AS started_at,
     date(to_timestamp(started_at) ) as date_started_at,
+    hour(to_timestamp(started_at)) as hour_started_at,
+    case 
+    when dayname(to_timestamp(started_at)) in ('sat','sun')
+    then 'weekend'
+    else 'businessday'
+    end as DAY_TYPE,
+
+    case when month(to_timestamp(started_at)) in (10,11,12)
+    then 'winter'
+    when month(to_timestamp(started_at)) in (1,2,3)
+    then 'spring'
+    when month(to_timestamp(started_at)) in (4,5,6)
+    then 'summer'
+    else 'rainy'
+    end as Station_of_year
+
 
     from
     {{source('demo','bike')}}
