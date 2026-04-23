bbf8de9d014ee94f19ea33b3285ba83350ea254d
amitesh@botconsulting.io <amitesh@botconsulting.io>
2026-04-23 10:51:21 +0000

    data_dimension_commit

--- macros/date_utils.sql
+++ macros/date_utils.sql
@@ -7,4 +7,42 @@ case when month(to_timestamp({{x}})) in (10,11,12)
     then 'summer'
     else 'rainy'
     end
+    {% endmacro %}
+
+{% macro day_type(x) %}
+case 
+    when dayname(to_timestamp(started_at)) in ('sat','sun')
+    then 'weekend'
+    else 'businessday'
+    end
+{% endmacro %}
+
+{% macro calc_trip_duration_minutes(started_at, ended_at) %}
+    ROUND(
+        DATEDIFF(second,
+            TRY_CAST({{ started_at }} AS TIMESTAMP),
+            TRY_CAST({{ ended_at }} AS TIMESTAMP)
+        ) / 60.0, 2
+    )
+{% endmacro %}
+
+{% macro extract_time_features(ts_col, prefix='') %}
+    DATE(TRY_CAST({{ ts_col }} AS TIMESTAMP)) AS {{ prefix }}date,
+    EXTRACT(YEAR  FROM TRY_CAST({{ ts_col }} AS TIMESTAMP))::INT AS {{ prefix }}year,
+    EXTRACT(MONTH FROM TRY_CAST({{ ts_col }} AS TIMESTAMP))::INT AS {{ prefix }}month,
+    EXTRACT(DOW   FROM TRY_CAST({{ ts_col }} AS TIMESTAMP))::INT AS {{ prefix }}day_of_week,   -- 0=Sunday
+    EXTRACT(HOUR  FROM TRY_CAST({{ ts_col }} AS TIMESTAMP))::INT AS {{ prefix }}hour,
+    CASE
+        WHEN EXTRACT(DOW FROM TRY_CAST({{ ts_col }} AS TIMESTAMP)) IN (0,6)
+        THEN TRUE ELSE FALSE
+    END
+{% endmacro %}
+
+{% macro normalize_rideable_type(rideable_type_col) %}
+    CASE LOWER(TRIM({{ rideable_type_col }}))
+        WHEN 'electric_bike' THEN 'Electric Bike'
+        WHEN 'classic_bike'  THEN 'Classic Bike'
+        WHEN 'docked_bike'   THEN 'Docked Bike'
+        ELSE 'Unknown'
+    END
 {% endmacro %}
--- models/date_dimensio.sql
+++ models/date_dimensio.sql
@@ -3,19 +3,17 @@ with cte as (
     to_timestamp(started_at) AS started_at,
     date(to_timestamp(started_at) ) as date_started_at,
     hour(to_timestamp(started_at)) as hour_started_at,
-    case 
-    when dayname(to_timestamp(started_at)) in ('sat','sun')
-    then 'weekend'
-    else 'businessday'
-    end as DAY_TYPE,
+    {{day_type('started_at')}} as DAY_TYPE,
 
-    {{get_session('started_at')}} as Station_of_year
-
+    {{get_session('started_at')}} as Station_of_year,
+    {{ calc_trip_duration_minutes('started_at', 'ended_at') }} AS trip_duration_minutes,
+    {{ extract_time_features('started_at', prefix='start_') }} AS Extracted_time,
+    {{ normalize_rideable_type('rideable_type') }} AS bike_type
 
     from
     {{source('demo','bike')}}
     where 
-    started_at !='started_at'
+    started_at !='started_at' or ended_at !='ended_at'
 
 )
 
