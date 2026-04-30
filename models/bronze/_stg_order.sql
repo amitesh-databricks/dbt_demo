2a3722dacb31edc7b9fc54eaca82bc9745438206
amitesh@botconsulting.io <amitesh@botconsulting.io>
2026-04-30 09:38:41 +0000

    dbt project all model connected

--- models/bronze/_stg_order.sql
+++ models/bronze/_stg_order.sql
@@ -4,6 +4,9 @@ SELECT
     payment_id::string as payment_id,
     order_id::string as order_id,
     unit_price::DECIMAL(10,2) as amount_eur,
+    order_date::Date as order_date,
+    status:: string as status,
+    customer_id::string as customer_id,
     {{ convert_eur_to_usd('unit_price') }} as amount_usd,
     currency::STRING as currency
 FROM {{ source('bronze', 'orders') }}
