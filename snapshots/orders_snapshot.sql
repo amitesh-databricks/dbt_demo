df0cd3e4f1ea7028dcb47838f31df1781ee5adfb
amitesh kumar <amitesh@botconsulting.io>
2026-05-12 09:17:41 +0000

    snapshot target schema change

--- snapshots/orders_snapshot.sql
+++ snapshots/orders_snapshot.sql
@@ -2,7 +2,7 @@
 
 {{
     config(
-      target_schema='snapshots',
+      target_schema='DBT_PROJECT_SCHEMA',
       unique_key='order_id',
       strategy='check',
       check_cols=['status', 'order_date'],
